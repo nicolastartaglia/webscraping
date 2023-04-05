ca33167ffcdd20df2fe6835e31d650d9a9dc4846
nitartaglia <nicolas.tartaglia@inrae.fr>
2023-04-05 17:11:29 +0200

    webscraping

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM node:18.14.2
+
+# Install latest chrome dev package and fonts to support major charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
+# Note: this installs the necessary libs to make the bundled version of Chromium that Puppeteer
+# installs, work.
+RUN apt-get update \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/googlechrome-linux-keyring.gpg \
+    && sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrome-linux-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-khmeros fonts-kacst fonts-freefont-ttf libxss1 \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/* \
+    && groupadd -r pptruser && useradd -rm -g pptruser -G audio,video pptruser
+
+RUN apt-get update && apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget x11vnc x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps xvfb
+
+USER pptruser
+
+RUN mkdir -p /home/pptruser/data
+RUN mkdir -p /home/pptruser/data/web
+RUN mkdir -p /home/pptruser/scripts
+
+WORKDIR /home/pptruser
+
+COPY puppeteer-browsers-latest.tgz puppeteer-latest.tgz puppeteer-core-latest.tgz ./
+
+# Install @puppeteer/browsers, puppeteer and puppeteer-core into /home/pptruser/node_modules.
+RUN npm i ./puppeteer-browsers-latest.tgz ./puppeteer-core-latest.tgz ./puppeteer-latest.tgz \
+    && rm ./puppeteer-browsers-latest.tgz ./puppeteer-core-latest.tgz ./puppeteer-latest.tgz 
+
+RUN npm i puppeteer xvfb
---
+++ scripts/webscrap.js
@@ -0,0 +1,99 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const dir = 'data/' + process.env.REP;
+const mySearch = process.env.SEARCH;
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+(async () => {
+
+    console.log('starting xvfb');
+    var Xvfb = require('xvfb');
+    var xvfb = new Xvfb({
+        xvfb_args: ['-screen', '0', '1600x1200x24+32']
+    });
+    xvfb.startSync();
+
+    console.log('xvfb started');
+
+    let browser = await puppeteer.launch({
+        headless: false,
+        devtools: true,
+        args: [
+            '--no-sandbox',
+            '--window-size=1440,1024',
+            '--window-position=0,0'
+        ],
+        userDataDir: './data/web'
+    });
+
+    const page = await browser.newPage();
+
+    console.log('Suppression des données de navigation');
+    const client = await page.target().createCDPSession();
+    await client.send('Network.clearBrowserCookies');
+    await client.send('Network.clearBrowserCache');
+
+    await page.setViewport({ width: 1440, height: 1024 });
+
+    console.log('Chargement de la page web Carrefour');
+    await page.goto('https://www.carrefour.fr', { waitUntil: ['networkidle2'] });
+
+    console.log("Clic sur la popup d'acceptation des cookies");
+    const buttonAcceptCookies = '#onetrust-accept-btn-handler';
+    await page.waitForSelector(buttonAcceptCookies);
+    await page.click(buttonAcceptCookies);
+
+    console.log("Recherche des produits correspondant à '" + mySearch + "'");
+    const searchInput = await page.$('[name="q"]');
+    await searchInput.type(mySearch);
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation({ waitUntil: ['networkidle2'] });
+
+    console.log("Récupération de tous les liens web des produits recherchés");
+    const selectorProduct = 'a.product-card-image';
+    const selectorIngredients = 'div.product-block-content > div > button';
+    const productLinkList = await page.$$eval(
+        selectorProduct,
+        (products => products.map(product => {
+            return { link: product.href, title: product.title }
+        }))
+    );
+
+    const next = "=================================================\n\n";
+    const sep = "-------------------------------------------------\n";
+    await fs.writeFile(dir+'/carrefour.txt', '', function (err) {
+        if (err) throw err;
+    });
+
+    for (const { link, title } of productLinkList) {
+
+        console.log("Chargement d'une page produit: " + link);
+        await page.goto(link, { waitUntil: ['networkidle2'] });
+
+        console.log("Temporisation aléatoire entre 8 et 12 secondes");
+        await page.waitForTimeout(getRandomInt(8000,12000));
+        const product = await page.waitForSelector(selectorIngredients);
+        if (product != null) await product.click();
+        const completeBlockIngredients = await page.waitForSelector('div.product-block-content > div > span');
+        await completeBlockIngredients.click();
+        const ingredients = await page.evaluate(el => el.innerText, completeBlockIngredients);
+        await fs.appendFile(dir + '/carrefour.txt', title + "\n" + sep + ingredients + "\n" + next, function (err) {
+            if (err) throw err;
+            console.log('Produit ajouté dans le fichier "Carrefour.txt"');
+        });
+    }
+    console.log('Fermeture du navigateur Chromium');
+    await browser.close();
+
+    xvfb.stopSync();
+})();
